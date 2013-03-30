e510a594e827a531f774501dc9410faa30d883c9
hhdys412 <307026423@qq.com>
2013-03-30 21:18:08 +0800

    update

--- TestAndroid/src/com/hhdys/testandroid/MainActivity.java
+++ TestAndroid/src/com/hhdys/testandroid/MainActivity.java
@@ -1,15 +1,32 @@
 package com.hhdys.testandroid;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	private ListView lv;
+	private EditText et;
+	ArrayAdapter<String> ad;
+	List<String> list;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		list = new ArrayList<String>();
+		ad = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+		et = (EditText) findViewById(R.id.editText1);
+		lv = (ListView) findViewById(R.id.listView1);
+		lv.setAdapter(ad);
 	}
 
 	@Override
@@ -17,6 +34,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	public void Add(View v) {
+		list.add(0, et.getText().toString());
+		ad.notifyDataSetChanged();
+		et.setText("");
 	}
 
 }
